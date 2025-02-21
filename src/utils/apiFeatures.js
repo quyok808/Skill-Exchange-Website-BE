4dab220c11bae33ba43bd94cb65d8ca8e07be68a
CongQuy252 <quyok25203@gmail.com>
2025-02-22 01:03:41 +0700

    Update sort(), filter()

--- src/utils/apiFeatures.js
+++ src/utils/apiFeatures.js
@@ -2,15 +2,39 @@ class APIFeatures {
   constructor(query, queryString) {
     this.query = query; // Mongoose query
     this.queryString = queryString; // req.query
+    this.mongoQuery = {};
   }
 
   filter() {
-    // Logic để lọc dựa trên req.query
+    const queryObj = { ...this.queryString }; // Tạo một bản sao của queryString
+
+    // Loại bỏ các trường dành riêng cho phân trang, sắp xếp, ...
+    const excludedFields = ["page", "limit", "sort", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let mongoQuery = {}; // Query mặc định (không có điều kiện)
+
+    if (queryObj.name) {
+      mongoQuery.name = { $regex: queryObj.name, $options: "i" }; // Tìm kiếm gần đúng (không phân biệt chữ hoa/thường)
+    }
+    if (queryObj.email) {
+      mongoQuery.email = { $regex: queryObj.email, $options: "i" }; // Tìm kiếm gần đúng (không phân biệt chữ hoa/thường)
+    }
+
+    this.mongoQuery = mongoQuery; // Lưu lại mongoQuery để đếm tổng số
+    this.query = this.query.find(mongoQuery); // Thêm điều kiện tìm kiếm
+
     return this; // Để chaining methods
   }
 
   sort() {
-    // Logic để sắp xếp
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" "); // Chuyển đổi "price,-createdAt" thành "price -createdAt"
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt"); // Sắp xếp mặc định theo createdAt giảm dần
+    }
+
     return this;
   }
 
